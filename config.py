535eb8259a60a7a654cad68dc161e2b22ea36d46
Yang Liu <foamliu@yeah.net>
2018-07-07 21:31:13 +0800

    update‘

--- config.py
+++ config.py
@@ -27,11 +27,11 @@ test_b_folder = 'data/ai_challenger_caption_test_b_20180103'
 train_annotations_filename = 'caption_train_annotations_20170902.json'
 valid_annotations_filename = 'caption_validation_annotations_20170910.json'
 
-# print('loading word embedding...')
-# from gensim.models import KeyedVectors
-#
-# zh_model = KeyedVectors.load_word2vec_format('data/wiki.zh.vec')
-# embedding_matrix = np.zeros((vocab_size, embedding_size))
-# for index, word in tqdm(enumerate(zh_model.vocab)):
-#     embedding_matrix[index, :] = zh_model[word]
-embedding_matrix = None
+print('loading word embedding...')
+from gensim.models import KeyedVectors
+
+zh_model = KeyedVectors.load_word2vec_format('data/wiki.zh.vec')
+embedding_matrix = np.zeros((vocab_size, embedding_size))
+for index, word in tqdm(enumerate(zh_model.vocab)):
+    embedding_matrix[index, :] = zh_model[word]
+
