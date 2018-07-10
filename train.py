3aa8c54421df576a1cde3406299d849dd78a26cc
Yang Liu <foamliu@yeah.net>
2018-07-10 16:16:27 +0800

    update

--- train.py
+++ train.py
@@ -68,5 +68,5 @@ if __name__ == '__main__':
                             verbose=1,
                             callbacks=callbacks,
                             use_multiprocessing=True,
-                            workers=4
+                            workers=get_available_cpus() // 2
                             )
