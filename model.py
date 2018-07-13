d340704a7c4c05a6e2d802330a13083c7cb68cc3
Yang Liu <foamliu@yeah.net>
2018-07-13 16:41:25 +0800

    update

--- model.py
+++ model.py
@@ -1,11 +1,10 @@
 import keras.backend as K
 import tensorflow as tf
-from keras.layers import Input, Dense, LSTM, Concatenate, Embedding, RepeatVector, Bidirectional, TimeDistributed, \
-    BatchNormalization
+from keras.layers import Input, Dense, CuDNNGRU, Concatenate, Embedding, RepeatVector, TimeDistributed, Dropout
 from keras.models import Model
 from keras.utils import plot_model
-from keras.applications.resnet50 import ResNet50
-from config import hidden_size, max_token_length, img_rows, img_cols, channel
+
+from config import max_token_length
 from config import vocab_size, embedding_size
 
 
@@ -13,24 +12,21 @@ def build_model():
     # word embedding
     text_input = Input(shape=(max_token_length,), dtype='int32')
     x = Embedding(input_dim=vocab_size, output_dim=embedding_size)(text_input)
-    x = LSTM(256, return_sequences=True)(x)
+    x = CuDNNGRU(512, return_sequences=True)(x)
+    x = Dropout(.5)(x)
     text_embedding = TimeDistributed(Dense(embedding_size))(x)
 
     # image embedding
-    image_encoder = ResNet50(input_shape=(img_rows, img_cols, channel), include_top=False, weights='imagenet', pooling='avg')
-    for layer in image_encoder.layers:
-        layer.trainable = False
-    image_input = image_encoder.layers[0].input
-    x = image_encoder.layers[-1].output
-    x = Dense(embedding_size, activation='relu', name='image_embedding')(x)
+    image_input = Input(shape=(2048,))
+    x = Dense(embedding_size, activation='relu', name='image_embedding')(image_input)
     # the image I is only input once
     image_embedding = RepeatVector(1)(x)
 
     # language model
     x = [image_embedding, text_embedding]
     x = Concatenate(axis=1)(x)
-    x = Bidirectional(LSTM(hidden_size, return_sequences=False))(x)
-
+    x = CuDNNGRU(512, return_sequences=False)(x)
+    x = Dropout(.5)(x)
     output = Dense(vocab_size, activation='softmax', name='output')(x)
 
     inputs = [image_input, text_input]
