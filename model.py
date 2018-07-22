4dd2cff3f3a90e92c0bc5a74e8b25888eb5b8d1c
Yang Liu <foamliu@yeah.net>
2018-07-22 17:56:14 +0800

    change lstm layer name

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ def build_model():
     # word embedding
     text_input = Input(shape=(max_token_length,), dtype='int32')
     x = Embedding(input_dim=vocab_size, output_dim=embedding_size)(text_input)
-    x = CuDNNLSTM(256, return_sequences=True, name='embedding_lstm')(x)
+    x = CuDNNLSTM(512, return_sequences=True, name='embedding_lstm')(x)
     text_embedding = TimeDistributed(Dense(embedding_size))(x)
 
     # image embedding
@@ -25,7 +25,7 @@ def build_model():
     x = [image_embedding, text_embedding]
     x = Concatenate(axis=1)(x)
     x = CuDNNLSTM(1024, return_sequences=True, name='language_lstm_1')(x)
-    x = CuDNNLSTM(512, name='language_lstm_2')(x)
+    x = CuDNNLSTM(1024, name='language_lstm_2')(x)
     output = Dense(vocab_size, activation='softmax', name='output')(x)
 
     inputs = [image_input, text_input]
