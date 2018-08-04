69683ed48174053e4db35ff2c4524069138870e7
Yang Liu <foamliu@yeah.net>
2018-08-04 13:55:22 +0800

    update

--- bleu_main.py
+++ bleu_main.py
@@ -30,54 +30,57 @@ class InferenceWorker(Process):
         Process.__init__(self, name='ImageProcessor')
         print("InferenceWorker init, GPU ID: {}".format(gpuid))
 
+        self.gpuid = gpuid
         self.in_queue = in_queue
         self.out_queue = out_queue
         self.signal_queue = signal_queue
 
     def run(self):
+        import tensorflow as tf
+        with tf.device(self.gpuid):
+            from nltk.translate.bleu_score import sentence_bleu
 
-        from nltk.translate.bleu_score import sentence_bleu
+            from beam_search import beam_search_predictions
+            from config import best_model, test_a_folder, test_a_annotations_filename
+            from model import build_model
 
-        from beam_search import beam_search_predictions
-        from config import best_model, test_a_folder, test_a_annotations_filename
-        from model import build_model
+            # load models
+            model = build_model()
+            model_weights_path = os.path.join('models', best_model)
+            model.load_weights(model_weights_path)
 
-        # load models
-        model = build_model()
-        model_weights_path = os.path.join('models', best_model)
-        model.load_weights(model_weights_path)
+            vocab = pickle.load(open('data/vocab_train.p', 'rb'))
+            idx2word = sorted(vocab)
+            word2idx = dict(zip(idx2word, range(len(vocab))))
 
-        vocab = pickle.load(open('data/vocab_train.p', 'rb'))
-        idx2word = sorted(vocab)
-        word2idx = dict(zip(idx2word, range(len(vocab))))
+            encoded_test_a = pickle.load(open('data/encoded_test_a_images.p', 'rb'))
 
-        encoded_test_a = pickle.load(open('data/encoded_test_a_images.p', 'rb'))
+            annotations_path = os.path.join(test_a_folder, test_a_annotations_filename)
 
-        annotations_path = os.path.join(test_a_folder, test_a_annotations_filename)
+            with open(annotations_path, 'r') as f:
+                annotations = json.load(f)
 
-        with open(annotations_path, 'r') as f:
-            annotations = json.load(f)
+            while True:
+                try:
+                    image_name = self.in_queue.get(block=False)
+                except queue.Empty:
+                    break
+                candidate = beam_search_predictions(model, image_name, word2idx, idx2word, encoded_test_a,
+                                                    beam_index=100)
+                image_hash = int(
+                    int(hashlib.sha256(image_name.split('.')[0].encode('utf-8')).hexdigest(), 16) % sys.maxsize)
+                reference = [anno['caption'].split() for anno in annotations['annotations'] if
+                             anno['image_id'] == image_hash]
+                score = sentence_bleu(reference, candidate)
+                self.out_queue.put(score)
+                # print('woker', self._gpuid, ' image_name ', image_name, " predicted as candidate", candidate)
+                # print('score: {}, remaining tasks: {} '.format(score, self.in_queue.qsize()))
 
-        while True:
-            try:
-                image_name = self.in_queue.get(block=False)
-            except queue.Empty:
-                break
-            candidate = beam_search_predictions(model, image_name, word2idx, idx2word, encoded_test_a, beam_index=100)
-            image_hash = int(
-                int(hashlib.sha256(image_name.split('.')[0].encode('utf-8')).hexdigest(), 16) % sys.maxsize)
-            reference = [anno['caption'].split() for anno in annotations['annotations'] if
-                         anno['image_id'] == image_hash]
-            score = sentence_bleu(reference, candidate)
-            self.out_queue.put(score)
-            # print('woker', self._gpuid, ' image_name ', image_name, " predicted as candidate", candidate)
-            # print('score: {}, remaining tasks: {} '.format(score, self.in_queue.qsize()))
+                self.signal_queue.put(SENTINEL)
 
-            self.signal_queue.put(SENTINEL)
-
-        # import keras.backend as K
-        # K.clear_session()
-        print('InferenceWorker done ')
+        import keras.backend as K
+        K.clear_session()
+        print('InferenceWorker done, GPU ID '.format(self.gpuid))
 
 
 class Scheduler:
@@ -125,8 +128,8 @@ def run(gpuids, q):
 
 
 if __name__ == "__main__":
-    # gpuids = ['/device:GPU:0', '/device:GPU:1', '/device:GPU:2', '/device:GPU:3']
-    gpuids = [0, 1, 2, 3]
+    gpuids = ['/device:GPU:0', '/device:GPU:1', '/device:GPU:2', '/device:GPU:3']
+    # gpuids = [0, 1, 2, 3]
     print(gpuids)
 
     q = mp.Queue()
