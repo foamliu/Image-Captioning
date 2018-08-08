8e686877340be18dd07abaee9066965210dc4d62
Yang Liu <foamliu@yeah.net>
2018-08-08 17:48:40 +0800

    udpate

--- caption_eval/run_evaluations.py
+++ caption_eval/run_evaluations.py
@@ -19,9 +19,8 @@
 
 import argparse
 import sys
-import importlib
-importlib.reload(sys)
-sys.setdefaultencoding('utf8')
+#reload(sys)
+#sys.setdefaultencoding('utf8')
 from coco_caption.pycxtools.coco import COCO
 from coco_caption.pycxevalcap.eval import COCOEvalCap
 
